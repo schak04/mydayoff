aecbb90673b19247e3603f7589626cb538aac895
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2026-02-26 00:09:31 +0530

    fix: uncomment logs for production debugging

--- server/server.js
+++ server/server.js
@@ -81,6 +81,6 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    // console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
 
